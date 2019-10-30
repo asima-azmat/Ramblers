9a9dba1012441133ac83ee1ccee73b8992c85794
aliciacisnerosm <aliciacisnerosm@gmail.com>
2019-10-30 15:42:15 +0100

    Routing initial screens

--- taskie/src/index.js
+++ taskie/src/index.js
@@ -3,7 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
+import { Route, Link, BrowserRouter, Switch as Router } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
+import Signup from "./screens/Signup.js";
+import Home from "./screens/Home.js";
+import NewUser from "./screens/CreateUser.js";
+
+const routing = (
+  <Router>
+    <div>
+      <Route path="./" component={App} />
+      <Route path="./NewUser" component={NewUser} />
+      <Route path="./home" component={Home} />
+    </div>
+  </Router>
+);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
