2355191477e725987f82d0143d9245122083c34a
aliciacisnerosm <aliciacisnerosm@gmail.com>
2019-11-12 11:33:22 +0100

    Add missing fields to the task form

--- taskie/src/screens/TaskForm.js
+++ taskie/src/screens/TaskForm.js
@@ -1,5 +1,7 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import firebase, { db } from "firebase";
+import css from "../css/taskform.css";
+import { Link, BrowserRouter } from "react-router-dom";
 
 class TaskForm extends Component {
   constructor(props) {
@@ -9,23 +11,66 @@ class TaskForm extends Component {
       description: "",
       relatedRole: "",
       deadline: "",
-      estimatedTime: ""
+      estimatedTime: "",
+      userid: firebase.auth().currentUser.uid,
+      team: "",
+      company: "",
+      createdBy: "",
+      assignedTo: ""
     };
   }
 
+  componentDidMount = () => {
+    var doc = firebase
+      .firestore()
+      .collection("User")
+      .doc(this.state.userid);
+
+    doc
+      .get()
+      .then(doc => {
+        this.setState({
+          company: doc.data().company,
+          team: doc.data().team,
+          createdBy: this.state.userid
+        });
+      })
+      .catch(function(error) {
+        console.log("Error getting document:", error);
+      });
+  };
 
   changeHandler = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
+
   submitHandler = event => {
     event.preventDefault();
-    const { taskTitle, description, relatedRole, deadline, estimatedTime } = this.state;
-    console.log(taskTitle, description, relatedRole, deadline, estimatedTime);
+    console.log(this.props);
+    const {
+      taskTitle,
+      description,
+      relatedRole,
+      deadline,
+      estimatedTime,
+      company,
+      team,
+      createdBy,
+      assignedTo
+    } = this.state;
     firebase
       .firestore()
       .collection("Task")
       .add({
-        taskTitle, description, relatedRole, deadline, estimatedTime
+        taskTitle,
+        description,
+        relatedRole,
+        deadline,
+        estimatedTime,
+        company,
+        team,
+        createdBy,
+        assignedTo
       })
       .then(docRef => {
         this.setState({
@@ -33,39 +78,60 @@ class TaskForm extends Component {
           description: "",
           relatedRole: "",
           deadline: "",
-          estimatedTime: ""
-   
+          estimatedTime: "",
+          company: "",
+          team: "",
+          createdBy: "",
+          assignedTo: ""
         });
+        console.log(this.props);
+        this.props.history.push("/Home");
       })
       .catch(error => {
         console.error("Error adding document: ", error);
       });
   };
   render() {
-    console.log("fuck you");
     return (
       <div className="task">
         <form onSubmit={this.submitHandler}>
           <h1>Task form!</h1>
           <label>Task title: </label>
+          <br></br>
           <input type="text" name="taskTitle" onChange={this.changeHandler} />
           <br></br>
           <label>Task Description: </label>
+          <br></br>
           <input type="text" name="description" onChange={this.changeHandler} />
           <br></br>
           <label>Related Role: </label>
+          <br></br>
           <input type="text" name="relatedRole" onChange={this.changeHandler} />
           <br></br>
           <label>Deadline: </label>
+          <br></br>
           <input type="text" name="deadline" onChange={this.changeHandler} />
           <br></br>
           <label>Estimated Time: </label>
-          <input type="text" name="estimatedTime" onChange={this.changeHandler} />
+          <br></br>
+          <input
+            type="date"
+            name="estimatedTime"
+            onChange={this.changeHandler}
+          />
           <br></br>
           {/* to be edited later.. 
           we also need to add one field to the state */}
-          <p>Assigned by: [whoever creates the task]</p> 
-          <input name="submit" type="submit" value="Send the Help Request"></input>
+          <p>
+            Assigned by:
+            <br></br>
+            {this.state.userid}
+          </p>
+          <input
+            name="submit"
+            type="submit"
+            value="Send the Help Request"
+          ></input>
         </form>
       </div>
     );
