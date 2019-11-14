f6baa81919f6aeefe5ead8dad2f8077d2c049003
aliciacisnerosm <aliciacisnerosm@gmail.com>
2019-11-14 14:47:20 +0100

    Remove unnecessary imports and add firebase queries to get popups

--- taskie/src/screens/Dashboard.js
+++ taskie/src/screens/Dashboard.js
@@ -1,24 +1,64 @@
 import React, { Component } from "react";
-//import firebase, { db } from "firebase";
 import { BrowserRouter as Redirect, Router, Route } from "react-router-dom";
+import firebase, { db } from "firebase";
 import Typography from "@material-ui/core/Typography";
 import css from "../css/dashboard.css";
 import { red } from "@material-ui/core/colors";
 import Navbar from "../components/Navbar";
 import SideBar from "../components/Sidebar";
-import TaskForm from "./TaskForm";
-import CreateUser from "./CreateUser";
 import addicon from "../assets/addicon.png";
 import { Link, BrowserRouter } from "react-router-dom";
 import Task from "../components/Task";
+import ControlledPopup from "../components/ControlledPopup";
 
 class Dashboard extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      userid: firebase.auth().currentUser.uid,
+      notification: false,
+      listenToSnapshot: false
+    };
   }
-  componentDidMount = () => {};
-  
+  componentDidMount = () => {
+    var doc = firebase
+      .firestore()
+      .collection("User")
+      .doc(this.state.userid);
+
+    doc
+      .get()
+      .then(doc => {
+        let that = this;
+
+        firebase
+          .firestore()
+          .collection("Task")
+          .where("company", "==", doc.data().company)
+          .where("team", "==", doc.data().team)
+          .onSnapshot(function(snapshot) {
+            if (
+              that.state.listenToSnapshot &&
+              doc.data().createdby != firebase.firestore().currentuser.uid
+            ) {
+              snapshot.docChanges().forEach(function(change) {
+                if (
+                  change.type === "added" &&
+                  change.doc.data().createdBy != doc.data().userid
+                ) {
+                  console.log("New task: ", change.doc.data());
+                  that.setState({ notification: true });
+                }
+              });
+            }
+            that.setState({ listenToSnapshot: true });
+          });
+      })
+      .catch(function(error) {
+        console.log("Error getting document:", error);
+      });
+  };
+
   render() {
     return (
       <div className="app">
@@ -27,6 +67,7 @@ class Dashboard extends Component {
           <div className="bar">
             <SideBar></SideBar>
           </div>
+          {this.state.notification ? <ControlledPopup></ControlledPopup> : null}
           <div className="dashboard">
             <div className="column">
               <div className="help-title">
@@ -43,8 +84,8 @@ class Dashboard extends Component {
                   </button>
                 </Link>
               </div>
-                <Task taskStatus="Help"></Task>
-              </div>
+              <Task taskStatus="Help"></Task>
+            </div>
             <div className="column">
               <div className="accepted-title">
                 <Typography component="h1" variant="h6" color="inherit" noWrap>
