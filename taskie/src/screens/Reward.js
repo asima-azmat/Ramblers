4752ad64cea9f73113d37fd8624c3150e19232a4
asima <ga27jor@mytum.de>
2019-11-21 00:55:32 +0100

    structure of reward system

---
+++ taskie/src/screens/Reward.js
@@ -0,0 +1,19 @@
+
+
+function Reward() {
+    return(
+        <div className="app">
+            <Navbar></Navbar>
+            <div className="screen">
+                <div className="bar">
+                    <SideBar></SideBar>
+                </div>
+                <div className="dashboard">
+                    
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Reward;
